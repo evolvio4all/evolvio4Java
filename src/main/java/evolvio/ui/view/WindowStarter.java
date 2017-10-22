2b72932ccb0927afa5c326b00276a076461a8e13
Nathan <n8style@yahoo.com>
2017-10-22 12:16:07 +0100

    Added requested changes

--- src/main/java/evolvio/ui/view/WindowStarter.java
+++ src/main/java/evolvio/ui/view/WindowStarter.java
@@ -1,11 +1,10 @@
-package evolvio.render;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+package evolvio.ui.view;
 
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 @Service
 public class WindowStarter {
@@ -15,6 +14,7 @@ public class WindowStarter {
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         frame.setSize(width, height);
         frame.setLocationRelativeTo(null);
+        frame.setTitle("Evolution simulator");
         frame.setVisible(true);
     }
 }
