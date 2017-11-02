4f003e4031f2e3d81b69f97d221f5d15cc6451f1
Nathan <n8style@yahoo.com>
2017-11-02 16:59:59 +0000

    Minor spelling correction

--- src/main/java/evolvio/io/keyboard/KeyAction.java
+++ src/main/java/evolvio/io/keyboard/KeyAction.java
@@ -7,9 +7,9 @@ import java.util.Map;
 import org.lwjgl.glfw.GLFW;
 
 public enum KeyAction {
-    Press(GLFW.GLFW_PRESS),
-    Repeat(GLFW.GLFW_REPEAT),
-    Release(GLFW.GLFW_RELEASE);
+    PRESS(GLFW.GLFW_PRESS),
+    REPEAT(GLFW.GLFW_REPEAT),
+    RELEASE(GLFW.GLFW_RELEASE);
     private static final Map<Integer, KeyAction> KEY_ACTIONS_BY_CODE = new HashMap<>();
     // the values() method re-creates a new array every time it's called, so cache it here
     private static final List<KeyAction> ALL = Arrays.asList(values());
